671ea3eec0de0e8be94dc017f2520a542bf0aef6
Kedar Nath Thakur <kedar.nath.thakur@gmail.com>
2025-12-20 23:57:46 +0530

    min resources part v2.0.33

--- static/theme2/js/custom/dashboard/paymentReportContent.js
+++ static/theme2/js/custom/dashboard/paymentReportContent.js
@@ -788,8 +788,6 @@ function filterStudentPaymentReportForm(){
 										+'<option value="25">25</option>'
 										+'<option value="50">50</option>'
 										+'<option value="100">100</option>'
-										+'<option value="150">150</option>'
-										+'<option value="200">200</option>'
 									+'</select>'
 								+'</div>'
 							+'</div>'
