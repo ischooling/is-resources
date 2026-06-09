66596ceddd2f7a886934b182caadf33e0b8d5352
Kedar Nath Thakur <kedar.nath.thakur@gmail.com>
2026-06-09 15:54:46 +0530

    min resources part v4.0.37

--- static/theme2/js/custom/studentExaminationSheet.js
+++ static/theme2/js/custom/studentExaminationSheet.js
@@ -3,20 +3,20 @@ var STUDENT_EXAMINATION_SHEET_FILE_PATH = "NA";
 async function renderStudentExaminationSheetPage(title){
     var filePath = await studentExaminationSheetFetchFilePath();
     STUDENT_EXAMINATION_SHEET_FILE_PATH = filePath;
-    console.log("renderStudentExaminationSheetPage :: filePath :: ", filePath);
-    await testPdfViewerEndpoint(filePath);
+    // console.log("renderStudentExaminationSheetPage :: filePath :: ", filePath);
+    await getPdfViewerEndpoint(filePath);
     $("#dashboardContentInHTML").html(getStudentExaminationSheetContent(title || "Student's Examination Schedule", filePath));
 }
 
-async function testPdfViewerEndpoint(filePath){
+async function getPdfViewerEndpoint(filePath){
     try {
         var url = APP_BASE_URL + "pdf-viewer?file=" + encodeURIComponent(filePath);
-        console.log("testPdfViewerEndpoint :: calling url :: ", url);
+        // console.log("testPdfViewerEndpoint :: calling url :: ", url);
         var response = await fetch(url);
-        console.log("testPdfViewerEndpoint :: status :: ", response.status);
-        console.log("testPdfViewerEndpoint :: content-type :: ", response.headers.get("content-type"));
+        // console.log("testPdfViewerEndpoint :: status :: ", response.status);
+        // console.log("testPdfViewerEndpoint :: content-type :: ", response.headers.get("content-type"));
         var text = await response.url;
-        console.log("testPdfViewerEndpoint :: response (first 500 chars) :: ", text.substring(0, 500));
+        // console.log("testPdfViewerEndpoint :: response (first 500 chars) :: ", text.substring(0, 500));
     } catch(e) {
         console.error("testPdfViewerEndpoint :: error :: ", e);
     }
@@ -43,7 +43,5 @@ async function studentExaminationSheetFetchFilePath(){
 function getPdfViewerUrl(pdfUrl) {
     return APP_BASE_URL +
         "static/pdfjs/web/viewer.html?file=" +
-        encodeURIComponent(
-            APP_BASE_URL + "pdf-proxy?url=" + encodeURIComponent(pdfUrl)
-        );
+        encodeURIComponent(APP_BASE_URL + "pdf-proxy?url=" + encodeURIComponent(pdfUrl));
 }
