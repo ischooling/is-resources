04fba982ae6528ee4f93bb81a423b5b3062e2a02
Kedar Nath Thakur <kedar.nath.thakur@gmail.com>
2026-06-16 18:25:52 +0530

    min resources part v4.0.49

--- static/theme2/js/custom/sessionOutPermissionContent.js
+++ static/theme2/js/custom/sessionOutPermissionContent.js
@@ -11,20 +11,20 @@ function sessionOutPermissionContent(data){
                     </button>*/''}
                 </div>
                 <div class="modal-body pt-0 pb-0 bg-transparent">
-                    <form id="continueSessionForm" name="continueSessionForm" method="post" autocomplete="off">
+                    <form id="continueSessionForm" name="continueSessionForm" method="post" autocomplete="off" class="custom-field-scope">
                         <div class="server-error">
                             <p id="serverError"></p>
                             <p id="messageDiv1"></p>
                             ${/*<p id="msgTheme2"></p>*/''}
                         </div>	
                         <div class="form-row form-session-out">
+                            <input type="hidden" name="location" id="location" value="{}"/>
+                            <input type="hidden" name="fromSpoof" id="fromSpoof" value="N"/>
+                            <input type="hidden" name="email" id="email" class="form-control" value="${data.emailId}" maxlength="50">
                             <div class="col-md-12">
-                                <div class="position-relative form-group mb-1 themeTwo">
-                                    <label class="text-white">Password <span style="color: red;">*</span></label> 
-                                    <input type="hidden" name="location" id="location" value="{}"/>
-                                    <input type="hidden" name="fromSpoof" id="fromSpoof" value="N"/>
-                                    <input type="hidden" name="email" id="email" class="form-control" value="${data.emailId}" maxlength="50">
-                                    <input name="password" id="password" class="form-control themeTow" autocomplete="off" type="password" maxlength="20"  value="" >
+                                <div class="position-relative form-group mb-1 themeTwo custom-field">
+                                    <input name="password" id="password" class="form-control themeTow" autocomplete="off" type="password" maxlength="20" value="" placeholder="Password">
+                                    <label class="input-floating-label" for="password">Password <span style="color: red;">*</span></label> 
                                     <span style="position: absolute; right: 0; bottom: 8px; display: inline-block; width: 35px; cursor: pointer" class="show-password">
                                         <i class="fa fa-eye-slash" id="icon-change" onclick="showPassWord('password', 'icon-change')"></i>
                                     </span>
@@ -32,11 +32,11 @@ function sessionOutPermissionContent(data){
                                 <p class="error" id="passwordError" style="color:red;font-weight:600"></p>
                             </div>
                             <div class="col-md-12">
-                                <div class="position-relative form-group mb-1 captcha-stylecss">
-                                    <label class="text-white">Captcha <span style="color: red;">*</span></label>
+                                <div class="position-relative form-group mb-1 captcha-stylecss custom-field">
                                     <div class="captcha-field">
-                                        <input type="text" name="captcha" id="captcha" class="form-control bg-white rounded" value="" pattern="^[_A-z0-9]{1,}$" minlength="6" maxlength="6"
+                                        <input type="text" name="captcha" id="captcha" class="form-control bg-white rounded-10" value="" placeholder="Enter captcha text" pattern="^[_A-z0-9]{1,}$" minlength="6" maxlength="6"
                                             onkeydown="return M.isAlphaNumericWithNoSpecialCharacter(event);" required>
+                                        <label class="input-floating-label" for="captcha">Captcha <span style="color: red;">*</span></label>
                                         <span class="captcha-img-wrapper show-password"> 
                                             <img id="captchaImage" alt="Captcha" src="">
                                         </span>
